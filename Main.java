925cf2cb1d23245c84c1482ecbbb2aebcc3bc212
Anastasia Lapshina <anapluslap@gmail.com>
2021-12-06 21:20:17 +0300

    Wrote the code for checking passwords

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String password;
+        System.out.println("Enter your password: ");
+        password = scanner.next();
+        scanner.close();
+        if (passwordCheck(password)) {
+            System.out.println("Your password is valid.");
+        } else {
+            System.out.println("Your password is invalid. Try again");
+        }
+
+    }
+
+    public static boolean passwordCheck(String s) {
+        return s.matches("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[_]).{8,}");
+    }
+}
